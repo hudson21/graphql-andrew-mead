030ef12090073748fcd6c517a2975adee35668d8
carlos.hudson <carlos.hudson@hp.com>
2019-09-03 15:09:24 -0500

    Lesson 14. Creating Custom Types 02:00

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,63 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga';
+
+
+//Type Definitions (schema)
+const typeDefs = `
+    type Query {
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
+    }
+`;
+
+//Resolvers
+const resolvers = {
+    Query: {
+        id() {
+            return 'abc123'
+        },
+        name() {
+            return 'Carlos Miguel Hudson Díaz'
+        },
+        age() {
+            return 23
+        },
+        employed() {
+            return true
+        },
+        gpa() {
+            return 9.5
+        },
+        title() {
+            return 'Assassins Creed'
+        },
+        price() {
+            return 99.99
+        },
+        releaseYear() {
+            return null
+        },
+        rating() {
+            return 5.4
+        },
+        inStock() {
+            return false
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The server is up!');
+});
