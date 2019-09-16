499beb10797993519c783c9981fecef6758278c7
carlos.hudson <carlos.hudson@hp.com>
2019-09-16 12:02:06 -0500

    Lesson 38. Expanding the Posts Subscription for Edits and Deletions 06:30

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,5 +1,5 @@
 const Subscription = {
-    count: {
+    /*count: {
         subscribe(parent, args, { pubsub }, info) {
             let count = 0;
 
@@ -13,7 +13,7 @@ const Subscription = {
             //channel name
             return pubsub.asyncIterator('count')
         }
-    },
+    },*/
     comment: {
         subscribe(parent, { postId }, { db, pubsub }, info) {
             const post = db.posts.find(post => post.id === postId && post.published)
@@ -22,7 +22,13 @@ const Subscription = {
             
             return pubsub.asyncIterator(`comment ${postId}`) // comment 44
         }
+    },
+    post: {
+        subscribe(parent, args, { pubsub }, info) {
+            return pubsub.asyncIterator('post')
+        }
     }
+
 } 
 
 export { Subscription as default }
